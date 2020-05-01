0248075a05f9c9cf50fc0159df2ac0a2bcb04b19
Eduardo Guillen <eguillen117@gmail.com>
2020-05-01 18:07:06 -0500

    Minor Push - Laid out Plan

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase ="abcdefghijklmnopqrstuvwxyz"
+
 
 // Write password to the #password input
 function writePassword() {
@@ -15,16 +17,30 @@ var questions = {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
+
+function getPassLength() {
+    var userPassLength = prompt(
+        "Enter your desired password length bewteen 8-20"
+    )
+}
+
+
 
 
 
 // Brain storming area
 
-//PLANNING
+//PLANNING AND MY PERSONAL NOTES
 
-// 1. ASK HOW MANY CHARACTERS
-// 2.
+// 1. PROMPT ASK HOW MANY CHARACTERS
+// 2. PROMT ASK IF THEY WANT NUMBERS
+// 3. PROMPT ASK IF THEY WANT SPECIAL CHARACTERS
+// 4. CREATE 4 SEPERATE FUNCTIONS. 1 WITH JUST ALPHABET, 2ND WITH ALHABET AND NUMBERS
+// 3RD. ALPHABET, NUMBERS AND CHARACTERS
+//5. KICK OUT WHAT IT GENERATES
+
+
 
 
 // function generatePassword() {
